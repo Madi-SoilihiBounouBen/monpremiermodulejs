2c513ebd59e6986e61562f21ce2a7de392d80ebd
Madi-SoilihiBounouBen <madisoilihi1999@gmail.com>
2026-01-19 16:28:58 +0300

    J'ai rajouté l'heure actuelle à mon site

--- generateHeure.js
+++ generateHeure.js
@@ -4,13 +4,23 @@
 
 //Je déclare une fonction qui retourne l'heure
 function monHeure() {
+    // Je crée une nouvelle instance de la date actuelle
     let date = new Date();
+    // J'extrais les heures de la date
     let heure = date.getHours();
+    // J'extrais les minutes de la date
+    let minute = date.getMinutes();
+    // J'extrais les secondes de la date
+    let seconde = date.getSeconds();
 
-    console.log("### Coucou, je passe à la télé à ", heure, "###");
+    // J'affiche un message dans la console avec l'heure, les minutes et les secondes
+    console.log("### Coucou, je passe à la télé à ", heure, ":", minute, ":", seconde, "###");
     
-    return heure;
+    // Je retourne un objet contenant les heures, minutes et secondes
+    return { heure, minute, seconde };
 }
 
 //Exporter ou exposer la fonction monHeure () parce que je veux rendre la fonction acccessible depuis un autre fichier JS
-export default { monHeure };
+export default { 
+    monHeure 
+};
--- main.js
+++ main.js
@@ -2,4 +2,27 @@
  * Ici, je réunirai tous les modules de mon projet
  */
 
-//J'importe la fonction monHeure depuis le fichier generateheure.js
+// J'importe le module depuis le fichier generateHeure.js
+import generateHeure from "./generateHeure.js";
+
+// Je récupère les éléments HTML pour afficher l'heure, minutes et secondes
+let heureActuelle = document.querySelector("#heure");
+
+// Je récupère l'élément HTML avec l'id "minutes"
+let minutesActuelle = document.querySelector("#minutes");
+
+// Je récupère l'élément HTML avec l'id "seconde"
+let secondeActuelle = document.querySelector("#seconde");
+
+// J'appelle la fonction monHeure() pour récupérer l'heure, minute et seconde actuelles
+let temps = generateHeure.monHeure();
+
+// J'affiche chaque valeur dans son span
+heureActuelle.textContent = temps.heure + "h ";
+// J'affiche les minutes dans le span "minutes"
+minutesActuelle.textContent = temps.minute + "m ";
+// J'affiche les secondes dans le span "seconde"
+secondeActuelle.textContent = temps.seconde + "s";
+
+// J'affiche un message dans la console avec l'heure complète
+console.log("Main - mon heure : ", `${temps.heure}h ${temps.minute}m ${temps.seconde}s`);
